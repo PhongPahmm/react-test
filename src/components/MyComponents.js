246a0efaad241adb91d647e881307e159e89150f
PhongPahm <phongphp03@gmail.com>
2024-07-14 11:01:11 +0700

    state continue

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -6,9 +6,18 @@ class MyComponent extends React.Component {
         address: 'HaNoi',
         age: '25'
     }
+    handleClick(event) {
+        this.setState({
+            name: 'daikphong',
+            age: Math.floor((Math.random() * 100) + 1)
+        }
+        )
+    }
     render() {
         return (
-            <div>Hello my name is {this.state.name} and  I'm from {this.state.address} and I'm {this.state.age} years old</div>
+            <div>Hello my name is {this.state.name} and  I'm {this.state.age} years old
+                <button onClick={(event) => this.handleClick(event)}>Click me</button>
+            </div>
         );
     }
 }
