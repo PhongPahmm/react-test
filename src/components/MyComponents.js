836c6ed46f8fb7d38e367f628c1e0f179b0b17f0
PhongPahm <phongphp03@gmail.com>
2024-07-14 09:42:26 +0700

    react component

---
+++ src/components/MyComponents.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>My first component</div>
+        );
+    }
+}
+export default MyComponent;
