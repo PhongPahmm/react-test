4b2cdbbf01a9277d6ddd5294b8d42b2008b7fdab
PhongPahm <phongphp03@gmail.com>
2024-07-14 10:00:46 +0700

    react state

--- src/App.js
+++ src/App.js
@@ -26,4 +26,23 @@ const App = () => {
   );
 }
 
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 export default App;
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'PhongPahm',
+        address: 'HaNoi',
+        age: '25'
+    }
     render() {
         return (
-            <div>My first component</div>
+            <div>Hello my name is {this.state.name} and  I'm from {this.state.address} and I'm {this.state.age} years old</div>
         );
     }
 }
