ed67e75acb3737f8509d074709c12544bf831596
PhongPahm <phongphp03@gmail.com>
2024-09-02 13:07:18 +0700

    sass and css in react

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './DisplayInfo.scss'
 class DisplayInfo extends React.Component {
 
     state = {
@@ -16,16 +16,16 @@ class DisplayInfo extends React.Component {
         const { listUsers } = this.props
         console.log(listUsers)
         return (
-            <div>
+            <div className="display-infor-container">
                 <button onClick={() => { this.handleShowHide() }}>
-                    {this.state.isShowList == true ? "Hide user's list" : "Show user's list"}
+                    {this.state.isShowList === true ? "Hide user's list" : "Show user's list"}
                 </button>
                 {this.state.isShowList &&
                     <div>
                         {listUsers.map((user, index) => {
                             return (
                                 <div key={index} className={+user.age > 18 ? "red" : "green"}>
-                                    <div>My name is {user.name}</div>
+                                    <div style={{ color: "yellow", paddingTop: "20px" }}>My name is {user.name}</div>
                                     <div>My age is {user.age}</div>
                                     <hr></hr>
                                 </div>
