bd7d2ffeddd49436c301dde7ebaf5a519ec43761
PhongPahm <phongphp03@gmail.com>
2024-09-07 22:08:04 +0700

    react hook update

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,43 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUserInfo from "./AddUserInfo";
 import DisplayInfo from "./DisplayInfo";
 
-class MyComponent extends React.Component {
-    state = {
-        listUsers: [
+// class MyComponent extends React.Component {
+//     state = {
+//         listUsers: [
+//             { id: 1, name: 'PhongPahm', age: '17' },
+//             { id: 2, name: 'PhongPahm2', age: '22' },
+//             { id: 3, name: 'PhongPahm3', age: '23' }
+//         ]
+//     }
+//     handleAddNewUser = (newUser) => {
+//         this.setState({
+//             listUsers: [...this.state.listUsers, newUser]
+//         })
+//     }
+//     handleDeleteUser = (userId) => {
+//         let tempListuser = this.state.listUsers
+//         tempListuser = tempListuser.filter(item => item.id !== userId)
+//         this.setState({
+//             listUsers: tempListuser
+//         })
+//     }
+//     render() {
+//         return (
+//             <>
+//                 <div className="a">
+//                     <AddUserInfo
+//                         handleAddNewUser={this.handleAddNewUser}
+//                     />
+//                     <hr></hr>
+//                     <DisplayInfo
+//                         listUsers={this.state.listUsers}
+//                         handleDeleteUser={this.handleDeleteUser}
+//                     />
+//                 </div>
+//                 <div className="b"></div>
+//             </>
+//         );
+//     }
+// }
+
+const MyComponent = (props) => {
+
+    const [listUsers, setListUsers] = useState(
+        [
             { id: 1, name: 'PhongPahm', age: '17' },
             { id: 2, name: 'PhongPahm2', age: '22' },
             { id: 3, name: 'PhongPahm3', age: '23' }
         ]
+    )
+
+    const handleAddNewUser = (newUser) => {
+        setListUsers([...listUsers, newUser])
     }
-    handleAddNewUser = (newUser) => {
-        this.setState({
-            listUsers: [...this.state.listUsers, newUser]
-        })
-    }
-    handleDeleteUser = (userId) => {
-        let tempListuser = this.state.listUsers
+    const handleDeleteUser = (userId) => {
+        let tempListuser = listUsers
         tempListuser = tempListuser.filter(item => item.id !== userId)
-        this.setState({
-            listUsers: tempListuser
-        })
+        setListUsers(tempListuser)
     }
-    render() {
-        return (
-            <>
-                <div className="a">
-                    <AddUserInfo
-                        handleAddNewUser={this.handleAddNewUser}
-                    />
-                    <hr></hr>
-                    <DisplayInfo
-                        listUsers={this.state.listUsers}
-                        handleDeleteUser={this.handleDeleteUser}
-                    />
-                </div>
-                <div className="b"></div>
-            </>
-        );
-    }
+
+    return (
+        <>
+            <div className="a">
+                <AddUserInfo
+                    handleAddNewUser={handleAddNewUser}
+                />
+                <hr></hr>
+                <DisplayInfo
+                    listUsers={listUsers}
+                    handleDeleteUser={handleDeleteUser}
+                />
+            </div>
+            <div className="b"></div>
+        </>
+    );
 }
+
 export default MyComponent;
