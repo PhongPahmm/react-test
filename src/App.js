69dad63d38b5a3dd8b122735826ceffadca79eda
PhongPahm <phongphp03@gmail.com>
2024-09-08 14:15:21 +0700

    react-router link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.scss';
+import Header from './components/Header/Header';
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
-      Hello World
+      <Header />
+      <div>
+        <button>
+          <Link to="/admins">Go to the admin page </Link>
+        </button>
+        <button>
+          <Link to="/users">Go to the user page </Link>
+        </button>
+      </div>
     </div>
   );
 }
