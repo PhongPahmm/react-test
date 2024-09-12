a0828e11fd5fdae5e6dea689c22a63cbc7293ddf
PhongPahm <phongphp03@gmail.com>
2024-09-12 20:02:30 +0700

    update

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,16 @@
+import SideBar from "./SideBar";
+import './Admin.scss'
+
 const Admin = (props) => {
     return (
-        <div>Admin component</div>
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBar />
+            </div>
+            <div className="admin-content">
+                dskfjsdkl
+            </div>
+        </div>
     )
 }
 export default Admin;
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -3,7 +3,9 @@ import imageHomePage from "../../assets/hero.webp"
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            <img src={imageHomePage}></img>
+            <div className="homepage-img">
+                <img src={imageHomePage}></img>
+            </div>
             <div className="homepage-content">
                 <div className="title-1">Make forms
                     worth filling out</div>
