4c9d1991d524972fce95740c68012deb98bf9998
PhongPahm <phongphp03@gmail.com>
2024-11-28 13:45:40 +0700

    manage state with redux

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -3,11 +3,13 @@ import { useNavigate } from 'react-router-dom';
 import './Login.scss'
 import { postLogin } from '../../services/ApiServices';
 import { toast } from 'react-toastify';
-
+import { useDispatch } from 'react-redux';
+import { doLogin } from '../../redux/action/userAction'
 const Login = (props) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const navigate = useNavigate()
+    const dispatch = useDispatch()
     const validateEmail = (email) => {
         return String(email)
             .toLowerCase()
@@ -29,6 +31,7 @@ const Login = (props) => {
 
         let data = await postLogin(email, password)
         if (data && data.EC === 0) {
+            dispatch(doLogin())
             navigate('/')
             toast.success(data.EM)
         } else {
--- src/redux/reducer/rootReducer.js
+++ src/redux/reducer/rootReducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import counterReducer from './counterReducer';
+import userReducers from './userReducer';
+import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
     counter: counterReducer,
+    user: userReducer
 });
 
 export default rootReducer;
